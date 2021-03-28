120313d2a4779db9cdb7bb45d9eb318d0d60ffa7
a.anufriev <anufriev.artem.mail@gmail.com>
2021-03-28 22:06:51 +0300

    Deleted Constants class and added constants to NumberListFragment

--- app/src/main/java/ru/park/anufriev/homework/NumberListFragment.java
+++ app/src/main/java/ru/park/anufriev/homework/NumberListFragment.java
@@ -3,7 +3,6 @@ package ru.park.anufriev.homework;
 
 import android.content.res.Configuration;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,8 +15,12 @@ import androidx.recyclerview.widget.RecyclerView;
 
 public class NumberListFragment extends Fragment {
 
-    private Integer itemsCount = Constants.DEFAULT_ITEM_COUNT;
     public static final String ITEMS_COUNT_KEY = "items_count_key";
+    public static final Integer DEFAULT_ITEM_COUNT = 100;
+    public static final Integer PORTRAIT_COLUMN_COUNT = 3;
+    public static final Integer LANDSCAPE_COLUMN_COUNT = 4;
+
+    private Integer itemsCount = DEFAULT_ITEM_COUNT;
 
     @Nullable
     @Override
@@ -31,17 +34,13 @@ public class NumberListFragment extends Fragment {
         RecyclerView recyclerView = view.findViewById(R.id.number_list);
 
         Integer spanCount = container.getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT
-                ? Constants.PORTRAIT_COLUMN_COUNT : Constants.LANDSCAPE_COLUMN_COUNT;
+                ? PORTRAIT_COLUMN_COUNT : LANDSCAPE_COLUMN_COUNT;
 
         recyclerView.setLayoutManager(new GridLayoutManager(view.getContext(), spanCount));
 
         ListDataSource dataSource = new ListDataSource(itemsCount);
         NumberListAdapter adapter = new NumberListAdapter(dataSource);
         recyclerView.setAdapter(adapter);
-
-        recyclerView.setPadding(20, 20, 20, 20);
-        recyclerView.setClipToPadding(false);
-        recyclerView.setClipChildren(false);
 
         view.findViewById(R.id.add_number_button).setOnClickListener(new View.OnClickListener() {
             @Override
